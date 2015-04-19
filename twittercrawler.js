d6e3448a8f3e34f61b6cc10b6ba2c4e665bddd3b
j0h4x0r <troyal.lee@gmail.com>
2015-04-19 14:39:38 -0400

    fix bug...now sqs working on my AWS

--- twittercrawler.js
+++ twittercrawler.js
@@ -67,7 +67,7 @@ var crawler = function(io) {
 						else{
 							var params = {
 								MessageBody: tweet.text, /* required */
-								QueueUrl: 'https://sqs.us-east-1.amazonaws.com/937582816189/tweets', /* required */
+								QueueUrl: 'https://sqs.us-west-2.amazonaws.com/582714090425/tweets', /* required */
 								DelaySeconds: 0,
 								MessageAttributes: {
 								    tweetID: {
@@ -78,7 +78,7 @@ var crawler = function(io) {
 							};
 							sqs.sendMessage(params, function(err, data) {
 								if (err) console.log(err, err.stack); // an error occurred
-								else     console.log('sent!');           // successful response
+								//else     console.log(data);           // successful response
 							});
 						}
 					});
--- worker.js
+++ worker.js
@@ -50,7 +50,6 @@ setInterval(function (){
 	sqs.receiveMessage(params, function(err, data) {
 		if (err) console.log(err, err.track);  // an error occurred
 		else{ // got a message from queue
-			console.log(data);
 			if (data.Messages == undefined ){
 				return;
 			}
