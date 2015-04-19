120ee0d8c3407d0a40b1dfadfd9ae346111cc5e1
j0h4x0r <troyal.lee@gmail.com>
2015-04-19 17:36:03 -0400

    Optimizing DB connection

--- routes/sentiment.js
+++ routes/sentiment.js
@@ -4,39 +4,40 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 
 var dburl = 'mongodb://54.201.53.120:27017/TweetMap';
+var col;
 
 var https = require('https');
+
+// connect db
+MongoClient.connect(dburl, function(err, db) {
+	if(err) {
+		console.log('Connecting to DB failed');
+	} else {
+		col = db.collection('tweets');
+		if(!col) {
+			console.log('Connecting to Collection failed');
+		}
+	}
+});
 
 router.post('/', function(req, res, next) {
 	if(req.get('x-amz-sns-message-type') == 'Notification') {
 		var tweet_id = JSON.parse(JSON.parse(req.body).Message)._id;
-		if(!tweet_id) {
-			console.log(req.body);
-		}
 		// extract sentiment info from DB
-		MongoClient.connect(dburl, function(err, db) {
-			if(err) {
-				console.log('Connecting to DB failed');
-			} else {
-				var col = db.collection('tweets');
-				if(col) {
-					col.findOne({_id: new ObjectID(tweet_id)}, function(err, doc) {
-						if(err || !doc) {
-							console.log('Fetching documents failed');
-						} else {
-							var data = {}
-							data._id = doc._id.toHexString();
-							data.sentiment = doc.sentiment;
-							// send sentiment to clients
-							var io = global.socketio;
-							io.emit('sentiment', data);
-						}
-					});
+		if(col) {
+			col.findOne({_id: new ObjectID(tweet_id)}, function(err, doc) {
+				if(err || !doc) {
+					console.log('Fetching documents failed');
 				} else {
-					console.log('Connecting to Collection failed');
+					var data = {}
+					data._id = doc._id.toHexString();
+					data.sentiment = doc.sentiment;
+					// send sentiment to clients
+					var io = global.socketio;
+					io.emit('sentiment', data);
 				}
-			}
-		});
+			});
+		}
 	} else if(req.get('x-amz-sns-message-type') == 'SubscriptionConfirmation') {
 		var subscribeURL = JSON.parse(req.body).SubscribeURL;
 		https.get(subscribeURL, function(res) {
